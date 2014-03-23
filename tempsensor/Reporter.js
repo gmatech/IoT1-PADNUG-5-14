b74b557232bac196c8f8ed415e0d6bdb8fef5579
Gerald Aden <gerald@gmatech.com>
2014-03-22 18:40:39 -0700

    Continued development

---
+++ tempsensor/Reporter.js
@@ -0,0 +1,11 @@
+﻿function Reporter() {
+}
+
+Reporter.prototype = {
+    report: function() {
+        //throw "Not Implemented";
+        console.log('reported');
+    }
+}
+
+module.exports.Reporter = Reporter;
--- tempsensor/service.js
+++ tempsensor/service.js
@@ -1,5 +1,9 @@
 var tempSensor = require('./fakeTempSensor');
 var tempReporter = require('./tempReporter');
+var reporterModule = require('./reporter');
+
+var reporter = new reporterModule.Reporter();
+reporter.report();
 
 setInterval(function() {
     var temp = tempSensor.readTemperature(function(temp) {
