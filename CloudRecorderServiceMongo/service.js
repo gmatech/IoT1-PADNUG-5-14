64b512c254cce600c84d6a41639f9216298653bc
Gerald Aden <gerald@gmatech.com>
2014-04-13 15:42:59 -0700

    Continued work on cloud recording service

--- CloudRecorderServiceMongo/service.js
+++ CloudRecorderServiceMongo/service.js
@@ -1,12 +1,19 @@
-﻿var mqtt = require('mqtt');
+﻿//var process = require('process');
+var mqtt = require('mqtt');
 var mongoose = require('mongoose');
 var configure = require('./configure');
+
+/*
+process.on('exit', function() {
+    console.log('Exiting');
+});
+*/
 
 configure.init();
 var mqttHost = configure.get('mqttHost');
 var mqttPort = configure.get('mqttPort');
 
-client = mqtt.createClient(mqttPort, mqttHost);
+var client = mqtt.createClient(mqttPort, mqttHost);
 
 client.subscribe('temperature');
 
@@ -22,16 +29,14 @@ var recordSchema = mongoose.Schema({
 });
 var Record = mongoose.model('Record', recordSchema);
 
-client.on('message', function(topic, message) {
-    console.log('message: ' + message);
-    console.log("connecting...");
-    mongoose.connect('mongodb://localhost:27017/sensors');
-    var db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', function callback () {
-        console.log("connected");
+console.log("connecting...");
+mongoose.connect('mongodb://localhost:27017/sensors');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+    client.on('message', function(topic, message) {
+        console.log('message: ' + message);
         var recordData = JSON.parse(message);
-        console.log('saving data: ' + recordData);
         var record = new Record(recordData);
         console.log('saving record: ' + record);
         record.save(function(err) {
@@ -40,8 +45,7 @@ client.on('message', function(topic, message) {
             } else {
                 console.log('save error: ' + err);    
             }
-            console.log("disconnecting ...");
-            mongoose.disconnect();
         });
     });
 });
+
