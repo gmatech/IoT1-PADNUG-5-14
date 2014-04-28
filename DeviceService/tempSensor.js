ad7a72da0d59a06952f46e3f16ee13912f78b7dd
Gerald Aden <gerald@gmatech.com>
2014-04-27 19:37:48 -0700

    Added real temperature sensor

---
+++ DeviceService/tempSensor.js
@@ -0,0 +1,12 @@
+﻿var temper1 = require('temper1');
+
+var devices = temper1.getDevices();
+
+//console.log("Devices found:" + devices);
+
+exports.readTemperature = function(err, callback) {
+    temper1.readTemperature(devices[0], function(err, value) {
+        //console.log("Result: " + value);
+        callback(value);
+    });
+}
